da01e3e2482c53fa093f6013f6d2ae5b130818c9
yuheong <yuhe_ong@hotmail.com>
2019-09-22 10:53:33 -0400

    Fix second page comparison

--- public/javascripts/update_display.js
+++ public/javascripts/update_display.js
@@ -1,24 +1,30 @@
 // Listens for new key-value pairs added to update ref of firebase
 var updateRef = firebase.database().ref('update');
 var positionRef = firebase.database().ref('pos/');
+var newPhoneRef = firebase.database().ref('newphone/');
+var loadPhone = false;
 
 updateRef.on('child_added', function (snapshot) {
     var newPhone = snapshot.val();
     var imgUrl = newPhone.imageurl;
     var currPosition;
-    //console.log('children: ' + snapshot.numChildren());
 
-    positionRef.once('value').then(function (snapshot) {
-        if (snapshot.exists()) {
-            currPosition = snapshot.child("current").val();
-            console.log('index: ' + currPosition);
-        } else {
-            console.log('current index not in db');
-        }
-    }).then(() => {
-        var imgId = "image" + currPosition;
-        document.getElementById(imgId).setAttribute("src", imgUrl);
-    }).catch(err => {
-        console.log('error :' + err);
-    });
+    if (!loadPhone) {
+        newPhoneRef.once('value').then(function (snapshot) {
+            if (snapshot.exists()) {
+                currPosition = snapshot.child("current").val();
+                console.log('index: ' + currPosition);
+            } else {
+                console.log('current index not in db');
+            }
+        }).then(() => {
+            loadPhone = true;
+            document.getElementById("image0").setAttribute("src", imgUrl);
+        }).catch(err => {
+            console.log('error :' + err);
+        });
+    } else {
+        document.getElementById("image1").setAttribute("src", imgUrl);
+    }
+
 });
--- routes/phones.js
+++ routes/phones.js
@@ -18,5 +18,40 @@ router.get('/', function(req, res, next) {
 });
 
 
+// POST to next phone
+router.post('/compare', function(req, res, next) {
+  var prevModel = req.body.model;
+  var prevImage = req.body.image;
+  writeNewData('yes', prevModel, prevImage);
+
+  let phoneModels = [];
+  var query = db.query('SELECT * FROM phone_models').spread((results) => {
+    for (let i = 0; i < results.length; i++) {
+      const element = results[i];
+      phoneModels.push(element);
+    }
+    console.log(results);
+    res.render('compare', { title: 'Rogers Immersive', phoneModels: phoneModels });
+  });
+});
+
+// PUT data in firebase db
+function writeNewData(value, phone, imageurl) {
+  var newPhoneRef = firebase.database().ref('newphone/');
+  newPhoneRef.set({
+    current: 1
+  })
+
+  var pushRef = firebase.database().ref('update/').push();
+  pushRef.set({
+      updated: value,
+      phone: phone,
+      imageurl: imageurl
+  }).then(() => {
+      //console.log('write success');
+  }).catch(err => {
+      console.log('error :' + err);
+  });
+}
 
 module.exports = router;
